e9da506570e6ee1a308706ed1a04705a4770b024
partha117 <1305117.pc@ugrad.cse.buet.ac.bd>
2021-05-22 12:56:55 -0400

    debug

--- Reformer/Electra_PreTrained.py
+++ Reformer/Electra_PreTrained.py
@@ -151,7 +151,7 @@ class BugDataset(Dataset):
                 lambda x: scratch_path + "Data/Report_Files/" + get_uuid(x) + ".txt").tolist()
         temp = file_reader(before_fix_ast_path, report_files)
         return \
-            self.tokenizer.encode_plus(temp, truncation=True, max_length=4032, padding=True, pad_to_multiple_of=4032)
+            self.tokenizer.encode_plus(temp, truncation=True, max_length=2046, padding=True, pad_to_multiple_of=2046)
 
 
 
