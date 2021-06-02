027a253d9c5770a89af24ace61abe06bb6c7e865
partha117 <1305117.pc@ugrad.cse.buet.ac.bd>
2021-06-02 12:19:05 -0400

    debug

--- Roberta/Extended/Electra_PreTrained.py
+++ Roberta/Extended/Electra_PreTrained.py
@@ -341,7 +341,6 @@ if __name__ == "__main__":
     args.start_step = 0
     checkpoint_last = os.path.join(args.output_dir, latest_checkpoint) if latest_checkpoint is not None else os.path.join(args.output_dir, 'checkpoint-last')
     temp_config = AutoConfig.from_pretrained(args.dis_model_name_or_path)
-    temp_config.is_decoder = False
     if os.path.exists(checkpoint_last) and os.listdir(checkpoint_last):
         args.gen_model_name_or_path = os.path.join(checkpoint_last, 'generator')
         logger.info("Generator Last Checkpoint {}".format(args.gen_model_name_or_path))
