45f70a54b7ddbeccda0612be2c23f59b7da44276
partha117 <1305117.pc@ugrad.cse.buet.ac.bd>
2021-07-10 14:23:27 -0400

    position classifier added

--- Classifier/TextClassifierCosinePerformanceTrack.py
+++ Classifier/TextClassifierCosinePerformanceTrack.py
@@ -4,8 +4,12 @@ from torch.utils.data import Dataset, DataLoader, WeightedRandomSampler
 import zlib
 import os
 import sys
+from scipy.spatial.distance import cdist
 sys.path.append(os.path.abspath("/home/partha9/EmbeddingProject"))
-from Classifier.TransformerClassifierElectra import ElectraClassification, ClassificationHead, BugDataset, get_combined_full_dataset, batch_parser
+from Reformer import ElectraUtil
+from Reformer.ElectraUtil import ElectraForPreTraining
+from Classifier.TextTransformerClassifier import ElectraClassification, ClassificationHead, get_combined_full_dataset, \
+    batch_parser, freeze_model
 from pathlib import Path
 import numpy as np
 import pickle
@@ -15,9 +19,53 @@ from sklearn.metrics import roc_auc_score, average_precision_score
 from tqdm import tqdm
 import torch.nn as nn
 import torch.nn.functional as F
-from transformers import AutoTokenizer, AutoModel
+from transformers import AutoTokenizer, AutoModel, AutoConfig
 import argparse
 from tree_sitter import Language, Parser
+
+
+class BugDataset(Dataset):
+
+    def __init__(self, project_name, dataframe, scratch_path=None, parser=None):
+        self.tmp = scratch_path
+        self.tmp += project_name.split("/")[-1] + "/"
+        # JAVA_LANGUAGE = Language('build/my-languages.so', 'java')
+        # self.parser = Parser()
+        # self.parser.set_language(JAVA_LANGUAGE)
+        self.dataset = dataframe.copy()
+        if not os.path.isdir(self.tmp):
+            Path(self.tmp).mkdir(parents=True, exist_ok=True)
+        for item in self.dataset.iterrows():
+            # print(item[1])
+            if not os.path.isfile(self.tmp + str(item[1]['cid']) + "_report.txt"):
+                file = open(self.tmp + str(item[1]['cid']) + "_report.txt", "w")
+                file.write(str(item[1]['report']))
+                file.close()
+            if not os.path.isfile(self.tmp + str(item[1]['cid']) + "_content.txt"):
+                file = open(self.tmp + str(item[1]['cid']) + "_content.txt", "w")
+                file.write(item[1]['file_content'])
+                file.close()
+        # self.dataset.drop(columns=['report', 'file_content'], inplace=True)
+        self.map = {name: index for index, name in enumerate(self.dataset.columns.tolist())}
+        self.dataset = self.dataset.to_numpy()
+        self.parser = parser
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def get_all_label(self):
+        return self.dataset[:, self.map['match']]
+
+    def get_match_value(self, indices):
+        return self.dataset[indices, self.map['match']]
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        features = self.dataset[idx]
+        reportfile_content = features[self.map['report']]
+        code_data = zlib.decompress(bytes.fromhex(features[self.map['file_content']])).decode()
+        return features[self.map['cid']], str(reportfile_content), code_data, features[self.map['match']]
 
 
 def get_positive_dataset(test_dataset):
@@ -44,103 +92,80 @@ def calculate_metrices(combined_full_dataset, positive_test_data, project_name, 
         dev = "cuda:0"
     else:
         dev = "cpu"
-    global_y_true = np.array([])
-    global_y_predicted = np.array([])
 
     JAVA_LANGUAGE = Language('/home/partha9/build/my-languages.so', 'java')
     parser = Parser()
     parser.set_language(JAVA_LANGUAGE)
-    positive_test_data = positive_test_data if len(positive_test_data) <= 100 else positive_test_data.sample(frac=100.0/len(positive_test_data),random_state=13)
+    positive_test_data = positive_test_data if len(positive_test_data) <= 100 else positive_test_data.sample(
+        frac=100.0 / len(positive_test_data), random_state=13)
     print("Data Length: {}".format(len(positive_test_data)))
 
     loop_tqdm = tqdm(range(len(positive_test_data)), leave=True)
     for i in loop_tqdm:
         print((i / len(positive_test_data)) * 100)
-        #ToDo: Verify K
+        # ToDo: Verify K
         test_dataset = get_sample_set(idx=i, positive_test_data=positive_test_data,
-                                      combined_full_dataset=combined_full_dataset, k=10)
-        test_bugdataset = BugDataset(project_name=project_name, dataframe=test_dataset, scratch_path=scratch_path, parser=parser)
+                                      combined_full_dataset=combined_full_dataset, k=30)
+        test_bugdataset = BugDataset(project_name=project_name, dataframe=test_dataset, scratch_path=scratch_path,
+                                     parser=parser)
         test_dataloader = DataLoader(test_bugdataset, batch_size=30, pin_memory=True, num_workers=1)
         # data = next(iter(test_dataloader))
-
-        y_true = np.array([])
-        y_predicted = np.array([])
+        all_code_embedding = None
+        all_report_embedding = None
+        true_positive_location = 0
         for batch in test_dataloader:
-            _,  combined_input, labels = batch
+            _, report_input, code_input, labels = batch
 
-            # # code_ast_tree = parser.parse(bytes(code_inputs, 'utf-8')).root_node.sexp()
-            # combined_input = batch_parser(code_inputs,report_inputs)
-
-            combined_input, labels = \
-                tokenizer.batch_encode_plus(combined_input, truncation=True, max_length=token_max_size, padding=True,
+            report_input, code_input = \
+                tokenizer.batch_encode_plus(report_input, truncation=True, max_length=token_max_size, padding=True,
                                             return_tensors='pt')[
-                    'input_ids'], torch.tensor(labels, dtype=torch.float64).to(dev)
+                    'input_ids'], \
+                tokenizer.batch_encode_plus(code_input, truncation=True, max_length=token_max_size, padding=True,
+                                            return_tensors='pt')[
+                    'input_ids']
             model.eval()
+            print("Labels", labels)
             with torch.no_grad():
-                eval_result = model(input_ids=combined_input.to(dev))
-                eval_result = torch.sigmoid(eval_result.view(-1).double())
-            #print(eval_result)
-            y_true = np.append(y_true, labels.cpu().numpy().ravel())
-            y_predicted = np.append(y_predicted, eval_result.cpu().numpy().ravel())
-
+                code_embedding = model(input_ids=code_input.to(dev))[0].mean(1).detach().numpy()
+                report_embedding = model(input_ids=report_input.to(dev))[0].mean(1).detach().numpy()
+                if all_code_embedding is None and all_report_embedding is None:
+                    all_code_embedding = code_embedding
+                    all_report_embedding = report_embedding
+                else:
+                    all_code_embedding = all_code_embedding.append(code_embedding)
+                    all_report_embedding = all_report_embedding.append(report_embedding)
+            true_positive_location += np.where(labels == 1)[0] if np.any(labels == 1) else len(labels)
         # mrr calculation
-        sorted_prediction_rank = np.argsort(-y_predicted)
-        print("y_predict", y_predicted)
-        sorted_prediction_value = np.array([y_true[item] for item in sorted_prediction_rank])
-        print(sorted_prediction_value)
-        lowest_retrieval_rank = (sorted_prediction_value == 0).argsort(axis=0)
-        print(lowest_retrieval_rank, lowest_retrieval_rank[0])
+
+        print("Code Shape", all_code_embedding.shape)
+        print("Report Shape", all_report_embedding.shape)
+        similarity = 1 - cdist(all_code_embedding, all_report_embedding, metric='cosine')
+        true_positive_value = similarity[0][true_positive_location]
+        rank = np.max(np.sum(similarity > true_positive_value, axis=-1) + 1)
+
+        print("Rank", rank)
         position_array.append({
             "BugId": positive_test_data.iloc[i]["bug_id"],
-            "position": (lowest_retrieval_rank[0] + 1)
+            "position": rank
         })
-        mrr_value = np.append(mrr_value, np.array(1.0 / (lowest_retrieval_rank[0] + 1)))
 
-        # average precision calculation
-        map_value = np.append(map_value, np.array(average_precision_score(y_true, y_predicted)))
-
-        # Top K calculation
-        sorted_label_rank = np.argsort(-y_true)
-        for position_k in range(0, 20):
-            common_item = np.intersect1d(sorted_prediction_rank[:(position_k + 1)],
-                                         sorted_label_rank[:(position_k + 1)])
-            if len(common_item) > 0:
-                top_k_counter[position_k] += 1
-
-        global_y_true = np.append(global_y_true, y_true)
-        global_y_predicted = np.append(global_y_predicted, y_predicted)
-
-    # calculation of map
-    mean_average_precision = np.mean(map_value)
-
-    # calculation of auc
-    auc_score = roc_auc_score(global_y_true, global_y_predicted)
-
-    # calculation of mrr
-    print("Mrr", mrr_value)
-    mean_reciprocal_rank = np.mean(mrr_value)
-
-    # calculation of top k
-    acc_dict = {}
-    print("Top K", top_k_counter)
-    for i, counter in enumerate(top_k_counter):
-        acc = counter / (len(positive_test_data))
-        acc_dict[i + 1] = round(acc, 3)
-
-    return acc_dict, mean_reciprocal_rank, mean_average_precision, auc_score, pd.DataFrame(position_array)
+    return pd.DataFrame(position_array)
 
 
 if __name__ == "__main__":
+    sys.modules['ElectraUtil'] = ElectraUtil
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_no', help='Model Number')
+    parser.add_argument("--checkpoint", default=None, type=str, help="")
     parser.add_argument('--scratch_path', help='Model Number')
     parser.add_argument('--batch_size', help='Model Number')
     parser.add_argument('--model_path', help='Model Number')
     parser.add_argument('--tokenizer_path', help='Model Number')
     parser.add_argument('--test_data_path', help='Model Number')
     parser.add_argument('--token_max_size', help='Model Number')
+    parser.add_argument('--electra', action='store_true', help="")
+    parser.add_argument('--state_dict', action='store_true', help="")
+    parser.add_argument('--config', default=None, type=str, help="")
     options = parser.parse_args()
-
 
     model_no = int(options.model_no)
     scratch_path = options.scratch_path
@@ -148,34 +173,44 @@ if __name__ == "__main__":
     tokenizer_path = options.tokenizer_path
     test_data_path = options.test_data_path
 
-
     token_max_size = int(options.token_max_size)
     batch_size = int(options.batch_size)
     dev = "cuda:0" if torch.cuda.is_available() else "cpu"
-    model = torch.load(model_path + "Model_{}".format(model_no))
+    if options.electra:
+        if options.state_dict:
+            temp_config = AutoConfig.from_pretrained(options.config)
+            temp_config.is_decoder = False
+            temp_config.output_hidden_states = True
+            full_base_model_dict = torch.load(options.model_path + options.checkpoint)
+            full_base_model = ElectraForPreTraining(temp_config)
+            full_base_model.load_state_dict(full_base_model_dict)
+            model = freeze_model(full_base_model.electra, options.model_name)
+        else:
+            full_base_model = torch.load(options.model_path + options.checkpoint)
+            model = freeze_model(full_base_model.electra, options.model_name)
+    else:
+        full_base_model = AutoModel.from_pretrained(options.model_path + options.checkpoint)
+        model = freeze_model(full_base_model, options.model_name)
+    model.to(dev)
     tokenizer = RobertaTokenizer(tokenizer_path + "tokenizer/aster-vocab.json",
                                  tokenizer_path + "tokenizer/aster-merges.txt")
 
-    project_list = ["AspectJ", "Birt", "Tomcat","SWT","JDT","Eclipse_Platform_UI"]
+    project_list = ["AspectJ", "Birt", "Tomcat", "SWT", "JDT", "Eclipse_Platform_UI"]
     all_result = None
     for project_name in project_list:
         dump_file = open(model_path + "Result_{}.txt".format(project_name), "w")
         print("Starting Project {}".format(project_name))
         test_dataset = pd.read_csv(test_data_path + "{}_test.csv".format(project_name))
         positive_test_data = get_positive_dataset(test_dataset)
-        acc_dict, mean_reciprocal_rank, mean_average_precision, auc_score, dataframe = calculate_metrices(test_dataset,
-                                                                                               positive_test_data,
-                                                                                               project_name, tokenizer,
-                                                                                               scratch_path=scratch_path)
+        dataframe = calculate_metrices(test_dataset,
+                                       positive_test_data,
+                                       project_name, tokenizer,
+                                       scratch_path=scratch_path)
 
         dataframe['project'] = project_name
         if all_result is None:
             all_result = dataframe
         else:
             all_result = pd.concat([all_result, dataframe])
-        acc_dict['mean_reciprocal_rank'] = mean_reciprocal_rank
-        acc_dict['mean_average_precision'] = mean_average_precision
-        acc_dict['auc_score'] = auc_score
-        json.dump(acc_dict, dump_file)
 
-    all_result.to_csv(model_path + "all_position_result.csv", index=False)
+    all_result.to_csv(model_path + "cosine_all_position_result.csv", index=False)
